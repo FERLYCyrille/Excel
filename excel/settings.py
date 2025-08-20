d74f2e8f20ca2e81d3ed6d7db390de452fa8cfd4
FERLYCyrille <136183079+FERLYCyrille@users.noreply.github.com>
2025-08-20 10:57:20 +0200

    Update settings.py

--- excel/settings.py
+++ excel/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0l*h@(-de3_vo4t&@wpl3+sgyqeq*&rmpnf08opdrz8%_h(3^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['excel-jso7.onrender.com']
+
 
 
 # Application definition
